fc8ef93ca6757c0b83a11c4007c46907a04c21aa
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-04-11 15:20:32 -0400

    exercise-3

--- exercise-3.sql
+++ exercise-3.sql
@@ -1,3 +1,7 @@
+-- Exercise 3: Sorting Data with ORDER BY
+-- Question:
+-- Write an SQL query to retrieve all employees' first names, last names, and salaries, sorted in descending order of salary.
+
 SELECT *
 FROM employees
 ORDER BY salary DESC
