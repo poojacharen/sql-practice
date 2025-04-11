bb65fd87c351c01fbae7f343fc143d68bfcff995
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-04-11 15:19:08 -0400

    exercise-1

--- exercise-1.sql
+++ exercise-1.sql
@@ -1,5 +1,8 @@
+-- Write an SQL query to retrieve the first name, last name, and salary of all employees in the IT department.
+
 SELECT first_name,
        last_name,
        salary
  FROM employees
  WHERE department = "IT";
+
