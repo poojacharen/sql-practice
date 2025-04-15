581209f58f9d9a578415dee0feb5bad3684e6bdf
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-04-15 15:08:04 -0400

    exercise-63.sql

--- exercise-63.sql
+++ exercise-63.sql
@@ -2,7 +2,9 @@
 -- **Question:**  
 -- Write a query to count how many employees are in each department and sort the result by the count in **descending order**.
 
-SELECT department, COUNT(*) AS employee_count
+SELECT 
+  department, 
+  COUNT(*) AS employee_count
 FROM employees
 WHERE department IS NOT NULL
 GROUP BY department
