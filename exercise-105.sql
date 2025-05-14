a8e802cf94ec042a79eafae22b7e369a00ea3cbb
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-05-14 14:41:42 -0400

    exercise-105

---
+++ exercise-105.sql
@@ -0,0 +1,12 @@
+-- Department with highest average salary
+
+SELECT d.name AS department, 
+       AVG(e.salary) AS avg_salary
+FROM employees e
+JOIN departments d ON e.department_id = d.id
+GROUP BY d.name
+ORDER BY avg_salary DESC
+LIMIT 1;
+
+
+-- Joining with the departments table helps name the department, and sorting the average salary descending lets us fetch the top.
