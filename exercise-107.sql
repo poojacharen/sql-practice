5cf3d158888d0d5c3a6709cd0e81d8c15501c70d
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-05-14 14:44:05 -0400

    exercise-107

---
+++ exercise-107.sql
@@ -0,0 +1,10 @@
+--  Top 3 highest paid employees
+
+SELECT name, 
+      salary
+FROM employees
+ORDER BY salary DESC
+LIMIT 3;
+
+
+-- Straightforward use of ORDER BY and LIMIT to get top earners.
