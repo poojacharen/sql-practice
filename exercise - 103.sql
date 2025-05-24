63ac1e03958b78ba30064d008a09babfc2240451
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-05-24 11:06:47 -0400

    Calculate running total of bonuses - For each employee, show the running total of bonuses ordered by bonus date.

--- exercise - 103.sql
+++ exercise - 103.sql
@@ -7,4 +7,4 @@ SELECT employee_id,
        SUM(bonus_amount) OVER (PARTITION BY employee_id ORDER BY bonus_date) AS running_total
 FROM bonuses;
 
--- Here we use a window function SUM() OVER (...) to create a running total by employee.
+-- Here we use a window function SUM() OVER (...) to create a running total by employee!
