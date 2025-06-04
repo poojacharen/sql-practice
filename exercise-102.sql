822ff2ef4d321d27020ac7cfeffdc798a16fe29e
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-06-04 13:01:21 -0400

    Show the highest paid employee in each department.

--- exercise-102.sql
+++ exercise-102.sql
@@ -12,4 +12,5 @@ JOIN (
 ) dept_max ON e.department_id = dept_max.department_id AND e.salary = dept_max.max_salary;
 
 
+
 -- We use an inner join with an aggregated subquery to fetch the employee whose salary matches the maximum salary for each department.
